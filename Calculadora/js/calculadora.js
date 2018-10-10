c82ba90857f7341e75f0717cb6438ad5b14d571b
avelasco13 <avelasco1994@gmail.com>
2018-10-10 17:27:01 +0000

    Input teclado

--- Calculadora/js/calculadora.js
+++ Calculadora/js/calculadora.js
@@ -2,15 +2,38 @@
 
 var txt1 = input.txt1;
 var txt2 = input.txt2;
+var primerNumero;
+var op = false;
+var operador;
 
 function escribir (n){
 
-  if(n = "C"){
-    input.txt1 = "";
-  }
+//   if (n == "C") {
+//   input.txt1 = "";
+//   input.txt2 = "";
+// }
+// txt1 = text1 + n;
+//
+// document.getElementById("1").value = "1";
+if (primerNumero == null){
+  primerNumero = n;
+  document.getElementById("parrafoNumeroUno").innerHTML = primerNumero;
+}else if (primerNumero != null && op == false){
+  primerNumero=primerNumero+n;
+  document.getElementById("parrafoNumeroUno").innerHTML = primerNumero;
+}else if (primerNumero != null && op == true){
+
+}
+
+//controlar longitud maxima 5
+   //Concatenaciona
+
 
 function arit (n){
-  
+
+  op = true;
+  operador = n;
+  document.getElementById("parrafoOperador").innerHTML = operador;
 }
 
 }
