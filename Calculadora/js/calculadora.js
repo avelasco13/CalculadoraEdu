fb3f7e2c22b2559bbda247de29112cecd686b3cf
avelasco13 <avelasco1994@gmail.com>
2018-10-16 15:20:19 +0000

    Funció llegirText

--- Calculadora/js/calculadora.js
+++ Calculadora/js/calculadora.js
@@ -1,41 +1,37 @@
 
 
-var txt1 = input.txt1;
-var txt2 = input.txt2;
-var primerNumero = null;
-var segonNumero = null;
-var op = false;
+
+var numeroIn = null;
 var operador;
 
-function escribir (n){
+function escriure (n)
+{
 
-//   if (n == "C") {
-//   input.txt1 = "";
-//   input.txt2 = "";
+// if(n == "=")
+// {
+//   llegirText(numeroIn);
 // }
-// txt1 = text1 + n;
-//
-// document.getElementById("1").value = "1";
-
-
-if (primerNumero != null)
+if (numeroIn != null)
   {
-  primerNumero=primerNumero+n;
-
-  document.getElementById("textinput1").value = primerNumero;
+    numeroIn=numeroIn+n;
+    if(n == "C")numeroIn=null;
+    document.getElementById("textinput1").value = numeroIn;
+  }else {
+    if(n == "C")
+    {
+      numeroIn=null;
+    }else{
+      numeroIn = n;
+      document.getElementById("textinput1").value = numeroIn;
+    }
   }
+}
 
+function llegirText ()
+{
+  numeroIn = "hola";
+  document.getElementById("textinput1").value = numeroIn;
 
-  if (segonNumero == null && primerNumero != null && op == true){
-    op = false;
-    segonNumero = n;
-    document.getElementById("parrafoNumeroDos").innerHTML = segonNumero;
-  }
-
-  if (primerNumero == null){ //&& op == false){
-    primerNumero = n;
-    document.getElementById("parrafoNumeroUno").innerHTML = primerNumero;
-  }
 
 
 }
